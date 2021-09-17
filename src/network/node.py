fa2b8cf0c205b13801b92c594780fba49b2e4d4f
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-09-17 15:42:38 +0300

    add node timeout and config.py

--- src/network/node.py
+++ src/network/node.py
@@ -6,10 +6,13 @@ from requests.adapters import HTTPAdapter
 # noinspection PyUnresolvedReferences
 from requests.packages.urllib3.util.retry import Retry
 
+from .config import Config
+
 
 class Node:
     def __init__(self):
-        self.ipfs_api_base_url = "http://127.0.0.1:5001/api/v0/"
+        self.ipfs_api_base_url = Config.ipfs_node_base_url
+        self.timeout = Config.request_timeout
         self.ipfs_session = self.setup_ipfs_session()
         self.ipfs_version = self.load_ipfs_version()
 
@@ -20,29 +23,40 @@ class Node:
         return ipfs_session
 
     def load_ipfs_version(self) -> dict:
-        return self.ipfs_session.post(urljoin(self.ipfs_api_base_url, "version"), params={"all": True}).json()
+        return self.ipfs_session.post(
+            urljoin(self.ipfs_api_base_url, "version"),
+            params={"all": True},
+            timeout=self.timeout,
+        ).json()
 
     def load_cid(self, cid: str) -> dict:
-        return self.ipfs_session.post(urljoin(self.ipfs_api_base_url, "block/get"), params={"arg": cid}).json()
+        return self.ipfs_session.post(
+            urljoin(self.ipfs_api_base_url, "block/get"),
+            params={"arg": cid},
+            timeout=self.timeout,
+        ).json()
 
     def create_cid(self, data: dict) -> str:
         bytes_data = json.dumps(data).encode()
         return self.ipfs_session.post(
             urljoin(self.ipfs_api_base_url, "block/put"),
-            files={"content": bytes_data}
+            files={"content": bytes_data},
+            timeout=self.timeout,
         ).json()
 
     def publish_to_topic(self, topic: str, data: dict):
         json_data = json.dumps(data)
         return self.ipfs_session.post(
             urljoin(self.ipfs_api_base_url, "pubsub/pub"),
-            params={"arg": [topic, json_data]}
+            params={"arg": [topic, json_data]},
+            timeout=self.timeout,
         ).text
 
     def subscribe_to_topic(self, topic: str):
         subscribe_stream = self.ipfs_session.post(
             urljoin(self.ipfs_api_base_url, "pubsub/sub"),
             params={"arg": topic},
-            stream=True
+            stream=True,
+            timeout=self.timeout,
         )
         return subscribe_stream
