1c18a370929faeff7fef9b08a8e7a553dc7e0d98
yoyo <hvuhsg6@gmail.com>
2022-01-25 23:47:38 +0200

    use hardcoded time for testing and developing

--- src/blockchain/hardcoded.py
+++ src/blockchain/hardcoded.py
@@ -10,5 +10,5 @@ __all__ = ["GENESIS_BLOCK", "developer_address"]
 developer_address = "A2tddJpzOWIp1Dv81mqn4WJ/UQAjmFLPINRnkt67zMMR"
 
 GENESIS_BLOCK = Block(
-    index=0, previous_hash="0", forger=developer_address, timestamp=time()
+    index=0, previous_hash="0", forger=developer_address, timestamp=0
 )
